fcdaecfca316b6b53b3f1cad62ba2a2e579f7c0e
Suraj Gojanur <surajgojanur@gmail.com>
2024-02-29 13:42:53 +0530

    s

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://surajgojanur:jHoyOKLj80kXKwix@cluster0.9lydbqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://surajgojanur:1234@cluster0.bcmchqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
